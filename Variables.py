95a5e4f3d488546fdcd786923db9634621df639d
ApolloZeroXx <xapollozerox@gmail.com>
2023-05-04 00:12:21 -0600

    ADD: homework

---
+++ Variables.py
@@ -0,0 +1,10 @@
+
+Nombre= "Roberto"
+Apellido="Rosales Serrato"
+Agos="20"
+
+Lista_de_Compras=("Leche", "Pan", "Huevo", "carne")
+
+print(f'{Nombre} {Apellido} {Agos}')
+print(Lista_de_Compras)
+
---
+++ variable.py
@@ -0,0 +1,8 @@
+first_name = "Luis"
+last_name = "Gonzalez"
+age = 35
+list_shopping = ("Milk", "Eggs", "Vegetables")
+
+print(f"{first_name} {last_name} {age}")
+print(first_name + " " + last_name + " " + str(age))
+print(list_shopping)
